4955b67a2301fc5a81654d94105c0b20afc0baf7
JamesDowney <james.m.downey30@gmail.com>
2022-12-01 00:33:14 -0700

    attempt 3

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,17 @@
 import { bufferToFile, fileToBuffer, print } from "kolmafia";
 
 export default function main(): void {
-  const input = fileToBuffer("input.txt").trim().split(/\n/g);
+  const input = fileToBuffer("input.txt").split(/\n/g);
   let count = 0;
   let solutionArray: number[] = [];
   input.forEach((element) => {
-    if (element !== undefined) {
+    if (element !== "" && element !== null && element !== undefined) {
       count += parseInt(element);
     } else {
       solutionArray.push(count);
       count = 0;
     }
   });
-  const solution = solutionArray.sort((a, b) => b - a)[0];
+  const solution = solutionArray.sort((a, b) => b - a)[0] ?? 0;
   bufferToFile(solution.toString(), "output.txt");
 }
